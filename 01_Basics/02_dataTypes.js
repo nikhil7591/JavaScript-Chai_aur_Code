005311bb2b85bb3679d24db3fe6b7000dc84e976
Nikhil kumar <131590056+nikhil7591@users.noreply.github.com>
2025-03-10 12:00:25 +0530

    Add files via upload

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello"); // we are using node.js, not browser
+
+let name = "nikhil"; // string
+let age = 20; // int
+let isLoogedIn = false; // boolean
+
+// premitive datatype
+
+// number -> 2 to power 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined -> not assign value
+// symbol -> for uniqueness 
+
+// object
+// null -> typeof object,
+// undefined -> typeof undefined
+
+console.log(typeof null);
+console.log(typeof undefined);
+

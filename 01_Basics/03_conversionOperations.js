6e95905d74025ee19f7c3c71546d895f64d429bc
Nikhil kumar <131590056+nikhil7591@users.noreply.github.com>
2025-03-22 05:50:47 +0000

    Operations descussion

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,28 +1,65 @@
-let score = "33";
+let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
-let valueInNumber = Number(score);
-console.log(typeof(valueInNumber));
-
-// "33" => 33 convert kar deta hai 
-// "33abc" => NaN number main convert karne ki kushis karta hai lakin nhai kar pata
+// "33" => 33
+// "33abc" => NaN
 //  true => 1; false => 0
-// null => 0; undefined => NaN
 
-let isLoggedIn = 1;
+let isLoggedIn = "hitesh"
+
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
 
-// 1 => true; 0=> false
-// "" =? false
-// "nikhil" => true
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
 
-let someNumber = 22
+let someNumber = 33
+
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// *********************** Operations ***********************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
